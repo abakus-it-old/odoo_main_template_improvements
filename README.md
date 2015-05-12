eaa264f70b9248db0ffa3871ee4c81ce7fe31106
Bernard Delhez <bernarddelhez@gmail.com>
2015-05-12 08:56:26 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Odoo main template improvements
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+   'name': "Odoo main template improvements",
+    'version': '1.0',
+    'depends': [],
+    'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'CSS',
+    'description': 
+    """
+    Odoo main template improvements
+    This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+    """,
+    'data': ['view/template.xml'],
+    'qweb': [],
+}
