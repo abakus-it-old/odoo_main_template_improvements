be3bcab6c0db0dea5cfa2a5c24c995dee2979bc8
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 14:31:25 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
    'name': "Odoo main template improvements",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': [],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
